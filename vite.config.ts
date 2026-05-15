44379a7ff54e0a3726a787fce10310d49e4bd2a4
bacellarafa <rafael.reis@ftd.com.br>
2026-05-15 17:01:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -17,10 +16,11 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/plano-adaptativo-ia/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
+    // Tailwind is not being actively used - do not remove them
     react(),
     tailwindcss(),
   ],
@@ -30,25 +30,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
-  build: {
-  "lib": {
-    "entry": [
-      "./src/index.ts"
-    ],
-    "formats": [
-      "es"
-    ],
-    "cssFileName": "style"
-  },
-  "rollupOptions": {
-    "output": {
-      "preserveModules": true,
-      "preserveModulesRoot": "src",
-      "entryFileNames": "[name].js"
-    }
-  }
-},
 })
